10295ba55a8e9c7f73fbeda5a036034db67ec8c4
aumthakkar <aumthakkar@yahoo.co.uk>
2026-03-18 09:40:03 +0000

    first commit with working helm chart for py6app

---
+++ src/py6app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+import time
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/info')
+def info():
+    return jsonify({
+       "time" : time.ctime(),
+       "host" : socket.gethostname(),
+       "env" : "dev",
+       "msg" : "Hi Pranav!"
+    })
+
+
+@app.route('/api/v1/healthz')
+def hello_world():
+    return jsonify({
+        "status" : "up"
+    })
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
